851774fd3009a041cee385aebeffb2bb9355036b
MarioRodriguez99 <marioisraelrodriguezr@gmail.com>
2025-05-04 04:25:17 -0400

    Add Entities

---
+++ Fantasy/Fantasy.Bakend/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Fantasy.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy.Bakend.Data
+{
+    public class DataContext : DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        //Creando indice unico para paises (Para que no se repitan) en el campo name usando el siguiente metodo:
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Aqui se indica el modelBuilder para en la clase Country con HasIndex en name para evitar repetir nombres en los paises:
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
